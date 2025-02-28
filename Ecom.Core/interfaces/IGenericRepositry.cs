54818c5990f5d5115eb4e75b84caeb70619f5810
Abdulrhman Madi <Abdulrhman.Madi2002@hotmail.com>
2025-02-28 09:56:13 +0200

    Add project files.

---
+++ Ecom.Core/interfaces/IGenericRepositry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.interfaces
+{
+    public interface IGenericRepositry<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, bool>>[] Includes);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+
+    }
+}
---
+++ Ecom.infrastructure/infrastructureRegisteration.cs
@@ -0,0 +1,27 @@
+﻿using Ecom.Core.interfaces;
+using Ecom.infrastructure.Data;
+using Ecom.infrastructure.Repositries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecom.infrastructure
+{
+    public static class infrastructureRegisteration
+    {
+        public static IServiceCollection infrastructureConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepositry<>));
+            //apply Unit OF Work
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //apply DbContext
+            services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+            });
+
+            return services;
+        }
+    }
+}
